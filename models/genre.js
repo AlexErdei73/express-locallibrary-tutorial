551a616fe7ee10c60a4153a753649ad587960c00
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-09-15 08:14:00 +0100

    Make book detail pages work

--- models/genre.js
+++ models/genre.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const GenreSchema = Schema({
-  name: { type: String, minLength: 3, maxLength: 100, required: true },
+	name: { type: String, minLength: 3, maxLength: 100, required: true },
 });
 
 GenreSchema.virtual("url").get(function () {
-  return `/catalog/genre/${this._id}`;
+	return `/catalog/genre/${this._id}`;
 });
 
-module.exports = mongoose.model("genre", GenreSchema);
+module.exports = mongoose.model("Genre", GenreSchema);
